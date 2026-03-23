9fadb32b525097bfa148a3a5417fc59dee3ee891
NureKukulenkoDenys <denys.kukulenko@nure.ua>
2026-03-23 16:48:58 +0200

    fix

--- Web/pages/_app.jsx
+++ Web/pages/_app.jsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "leaflet/dist/leaflet.css";
+import { Authorization } from "../components/authorization";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps}/>
+  return (<Authorization type="register" />  )
 }
