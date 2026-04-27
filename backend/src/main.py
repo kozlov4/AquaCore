24fcabc8809384d11694b8bd09082ca8e5655a2a
Serge Kozlov <serge.kozlov.dev@gmail.com>
2026-04-27 16:49:51 +0300

    fix: disable package mode in poetry

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,19 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI(
   title="AquaCore 🐠"
 )
 
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello index!",
+    }
 
 
 
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
