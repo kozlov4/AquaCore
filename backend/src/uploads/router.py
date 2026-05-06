81de34c96a771ec9bd1fdf99a28a7229d48f99c5
Serge Kozlov <serge.kozlov.dev@gmail.com>
2026-05-06 22:32:12 +0300

    Додав логіку додавання картинок до бд

--- backend/src/uploads/router.py
+++ backend/src/uploads/router.py
@@ -1,17 +1,30 @@
 import cloudinary.uploader
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.models.db_helper import db_helper
+from core.models import Image
 
 router = APIRouter(tags=["Uploads"])
 
 
 @router.post("/upload-image/")
-def upload_image(file: UploadFile = File(...)):
+async def upload_image(
+    file: UploadFile = File(...),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
     if file.content_type not in ["image/jpeg", "image/png", "image/webp"]:
         raise HTTPException(status_code=400, detail="Тільки JPEG, PNG або WEBP")
 
     try:
         result = cloudinary.uploader.upload(file.file, folder="aqua_projects")
-        return {"image_url": result.get("secure_url")}
+        secure_url = result.get("secure_url")
+
+        new_image = Image(image_url=secure_url)
+        session.add(new_image)
+        await session.commit()
+        await session.refresh(new_image)
+
+        return {"image_id": new_image.id, "image_url": secure_url}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Помилка Cloudinary: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Ошибка: {str(e)}")
